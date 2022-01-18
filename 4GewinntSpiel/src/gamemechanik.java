407558f9d1a496b70c87b2b24df851c7c0911a11
BuildTools <unconfigured@null.spigotmc.org>
2022-01-18 23:01:13 +0100

    push

--- 4GewinntSpiel/src/gamemechanik.java
+++ 4GewinntSpiel/src/gamemechanik.java
@@ -3,7 +3,7 @@ public class gamemechanik {
 
 	public static void main(String[] args) {
 	System.out.println("hier kommen Spielmechaniken");
-
+	System.out.println("mal gucken ob das geupdated wird");
 	}
 
 }
